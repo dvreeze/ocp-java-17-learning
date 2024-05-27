35c7a3c9a541b8eb755c576f863f181397952b98
Chris de Vreeze <chris.de.vreeze@caiway.net>
2024-05-27 19:53:27 +0200

    Enhanced chapter 10 documentation

--- doc/chapter10-streams.md
+++ doc/chapter10-streams.md
@@ -20,9 +20,11 @@ Some *static methods* of generic class `java.util.Optional<T>` are:
 * Factory method `of(T)`, returning a non-empty Optional, provided the passed value is not `null` (or else an NPE is thrown)
 * Factory method `ofNullable(T)`, returning a non-empty Optional if the given value is not `null`, and an empty Optional otherwise
 
+Note that `Optional.ofNullable(v)` is equal to `(v == null) ? Optional.empty() : Optional.of(v)`.
+
 Some *instance methods* of class `java.util.Optional<T>` are:
 * `isPresent()`, returning `true` if the Optional is not empty
-* `isEmpty()`, returning `true` if the Optional is empty
+* `isEmpty()`, returning `true` if the Optional is empty (equal to the negation of `isPresent()`)
 * `get()`, returning the contained element, but throwing a `NoSuchElementException` if the Optional is empty (note: prefer method `orElseThrow()` to this one)
 * `orElse(T)`, returning the contained element, and if empty, falling back to the parameter value
 * `orElseGet(Supplier<? extends T>)`, returning the contained element, and if empty, uses the given Supplier to obtain a value
@@ -31,8 +33,55 @@ Some *instance methods* of class `java.util.Optional<T>` are:
 * `ifPresent(Consumer<? super T>)`, running the given Consumer on the contained value, if any, and a no-op otherwise
 * `ifPresentOrElse(Consumer<? super T>, Runnable)`, like `ifPresent` but running the given Runnable if the Optional is empty
 
+Note that some methods above are easy to express in terms of other ones (e.g. `orElseThrow` without parameters in terms of `orElseThrow` taking one parameter).
+
 Some "Stream-related" *instance methods* (all but the first one looking very much like similar *Stream API methods*) are:
 * `stream()`
 * `filter(Predicate<? super T>)`
 * `map(Function<? super T, ? extends U>)`, returning an `Optional<U>`
 * `flatMap(Function<? super T, ? extends Optional<? extends U>>)`, returning an `Optional<U>`
+
+The methods above, except for the first one, are convenience methods. For example, `myOptional.filter(p)` is equal to
+`myOptional.stream().filter(p).findFirst()`.
+
+### Using streams
+
+A Java *stream* is a *sequence of data, that can be used only once*. So, unlike collections (especially immutable collections)
+that can be reused again and again, streams are much more *like iterators* than collections in that they are not reusable.
+
+For example:
+
+```java
+var stream = Stream.of(1, 2, 3, 4, 5);
+
+// Ok, using the stream the first time, which will print the 5 numbers
+stream.toList().forEach(System.out::println);
+
+// NOT OK! The stream is closed and cannot be used again!
+stream.toList().forEach(System.out::println); // Throws an IllegalStateException!
+```
+
+Streams offer a *functional programming style* in Java, but streams themselves are not "functional objects" (unlike
+immutable records, for example). This is very important to keep in mind! Whereas streams are not reusable, *Supplier*s
+of streams or *collections* are "stream factories" are reusable. So "stream pipelines" should normally have a very local scope.
+
+Put differently, with streams data is created only when needed. In other words, stream processing is an example of *lazy evaluation*.
+Data flows through the pipeline and does not come back after each "step".
+
+A *stream pipeline* consists of (at most) 3 parts:
+1. the stream *source*, from which the stream is created
+2. *intermediate stream operations* (optional), transforming the stream into another one (filtering/mapping/flat-mapping etc.)
+3. exactly one *terminal operation*, producing a result, after which the stream is closed and no longer available
+
+Above, *lazy evaluation* means the following:
+* Nothing is evaluated immediately when the intermediate stream operations are called
+  * Think of the chain of intermediate stream operations as a "transformation recipe" that not yet runs
+* Only when the terminal operation is called, *processing of the intermediate stream operations is triggered*
+
+Streams may be *finite* or *infinite*. This is not clearly visible from the code, but still very important to keep in mind!
+
+Some important differences between *intermediate operations* and the *terminal operation* are:
+* Each useful stream pipeline *ends with exactly 1 terminal operation*, but there can be *0 or more intermediate operations*
+* Intermediate operations *return a Stream*, whereas a terminal operation *does not return a Stream*
+* When calling an intermediate operation, it is *not yet evaluated*, whereas *on calling the terminal operation the pipeline runs*
+* After an intermediate operation call the stream is still valid, but *after the terminal operation the stream is closed* and no longer available for processing
--- src/chapter10/OptionalExample.java
+++ src/chapter10/OptionalExample.java
@@ -18,7 +18,11 @@ package chapter10;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Example showing type "Optional".
@@ -31,16 +35,97 @@ public class OptionalExample {
         return IntStream.of(numbers).average().stream().boxed().findFirst();
     }
 
+    public static Optional<Double> getAverageUsingAveragingCollector(int... numbers) {
+        return IntStream.of(numbers).boxed()
+                .collect(
+                        Collectors.collectingAndThen(
+                                Collectors.averagingInt(i -> i),
+                                v -> (numbers.length == 0) ? Optional.empty() : Optional.of(v))
+                );
+    }
+
+    public static Optional<Double> getAverageUsingSum(int... numbers) {
+        double sum = IntStream.of(numbers).asDoubleStream().sum();
+
+        return OptionalInt.of(numbers.length).stream().filter(cnt -> cnt > 0).boxed().map(cnt -> sum / cnt).findFirst();
+    }
+
+    public record NumbersSumAndCount(double sum, int count) {
+    }
+
+    public static Optional<Double> getAverageUsingReduce(int... numbers) {
+        return IntStream.of(numbers).asDoubleStream().mapToObj(i -> new NumbersSumAndCount(i, 1))
+                .reduce((x, y) -> new NumbersSumAndCount(x.sum() + y.sum(), x.count() + y.count()))
+                .map(v -> v.sum() / v.count());
+    }
+
+    public static Optional<Double> getAverageUsingTeeing(int... numbers) {
+        return IntStream.of(numbers).boxed().collect(
+                Collectors.teeing(
+                        Collectors.summingDouble(i -> (double) i),
+                        Collectors.counting(),
+                        (sum, cnt) -> (cnt == 0) ? Optional.empty() : Optional.of(sum / cnt)
+                )
+        );
+    }
+
+    public static Optional<Double> getAverageUsingSideEffects(int... numbers) {
+        final var sum = new AtomicLong(0); // Mutable, but still effectively final as reference
+
+        // Looping using Stream.iterate
+        Stream.iterate(0, i -> i < numbers.length, i -> i + 1).forEach(i -> {
+            sum.addAndGet(numbers[i]); // side-effect
+        });
+
+        return Optional.of((double) sum.get()).filter(ignored -> numbers.length > 0).map(s -> s / (numbers.length));
+    }
+
+    public static Optional<Double> getAverageUsingLimitAndSideEffects(int... numbers) {
+        final var sum = new AtomicLong(0); // Mutable, but still effectively final as reference
+
+        // Looping using Stream.iterate
+        Stream.iterate(0, i -> i + 1).limit(numbers.length).forEach(i -> {
+            sum.addAndGet(numbers[i]); // side-effect
+        });
+
+        return Optional.of((double) sum.get()).filter(ignored -> numbers.length > 0).map(s -> s / (numbers.length));
+    }
+
     public static void averageExample() {
         List.<List<Integer>>of(
                 List.of(1),
                 List.of(1, 2, 3, 4),
                 List.of(),
+                List.of(0),
                 List.of(2, 4, 6),
                 List.of(-4, 78, 23, 67, 1344, 47, -233, -3542, 4565)
         ).forEach(list -> {
             var optAverage = getAverage(list.stream().mapToInt(i -> i).toArray());
             System.out.printf("Numbers: %s. Optional average: %s%n", list, optAverage.map(i -> String.format("%.2f", i)));
+
+            if (!optAverage.equals(getAverageUsingAveragingCollector(list.stream().mapToInt(i -> i).toArray()))) {
+                throw new RuntimeException("Some inconsistency between 2 methods computing average values (1)");
+            }
+
+            if (!optAverage.equals(getAverageUsingSum(list.stream().mapToInt(i -> i).toArray()))) {
+                throw new RuntimeException("Some inconsistency between 2 methods computing average values (2)");
+            }
+
+            if (!optAverage.equals(getAverageUsingReduce(list.stream().mapToInt(i -> i).toArray()))) {
+                throw new RuntimeException("Some inconsistency between 2 methods computing average values (3)");
+            }
+
+            if (!optAverage.equals(getAverageUsingTeeing(list.stream().mapToInt(i -> i).toArray()))) {
+                throw new RuntimeException("Some inconsistency between 2 methods computing average values (4)");
+            }
+
+            if (!optAverage.equals(getAverageUsingSideEffects(list.stream().mapToInt(i -> i).toArray()))) {
+                throw new RuntimeException("Some inconsistency between 2 methods computing average values (5)");
+            }
+
+            if (!optAverage.equals(getAverageUsingLimitAndSideEffects(list.stream().mapToInt(i -> i).toArray()))) {
+                throw new RuntimeException("Some inconsistency between 2 methods computing average values (6)");
+            }
         });
     }
 
